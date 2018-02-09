cf998f6882bbcde5d0755223240d4f1fad3e0341
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-09 11:17:52 -0800

    rename class to make it more readable

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,19 @@
-class Anagram
+class Word
   def initialize (string)
     @string = string
   end
-
+#can use string as def object method.
+  def string
+    @string
+  end
+#string & second_string as reader methods?
+  # def second_string
+  #   @second_string
+  # end
+#finder will determine if words are anagrams or not. all the nitty gritty here.
   def finder
     @string.split("")
   end
-
 
 
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,7 +4,7 @@ require('anagram')
 
 describe ('anagram') do
   it("will split string into an array") do
-    word = Anagram.new("hi")
+    word = Word.new("hi")
     expect(word.finder()).to(eq(["h","i"]))
   end
 end
