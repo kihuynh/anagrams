d4d71861be9d01c41a33f95c4b30c2d15662a73f
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-02-09 15:41:38 -0800

    write third test to check for vowels, and fail

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Word
     word_1 = @word
     word_2 = word_two
 
+
     if word_1.downcase.chars.sort == word_2.downcase.chars.sort
       "It's an anagram"
     else
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,8 +7,14 @@ describe('anagram') do
     word = Word.new('tea')
     expect(word.finder('eat')).to(eq("It's an anagram"))
   end
+
   it('check if two words with DIFFERENCE CASES will still be an anagram') do
     word = Word.new('Spear')
     expect(word.finder('sPARE')).to(eq("It's an anagram"))
+  end
+
+  it('check if the word has vowels otherwise it will not work') do
+    word = Word.new('Despair')
+    expect(word.finder('brt')).to(eq("You'll need a vowel"))
   end
 end
